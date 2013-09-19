8201dcc8e7ecf6a16b998498c3ffdffd155cda07
Your Name Here <amyr@amyrhaq.com>
2013-09-19 16:34:05 -0700

    Added form has been submitted

--- simplephp.php
+++ simplephp.php
@@ -22,4 +22,38 @@ if(isset($_GET['logout']))
 {
 	unset($_SESSION['login']);
 }
+
+// ******************************************* //
+// **********	USER IS LOGGED IN	********** //
+// ******************************************* //
+
+if (isset($_SESSION['login']) && $_SESSION['login'] == $hash) {
+
+	?>
+			
+		<p>Hello <?php echo $username; ?>, you have successfully logged in!</p>
+		<a href="?logout=true">Logout?</a>
+			
+	<?php
+}
+// *********************************************** //
+// **********	FORM HAS BEEN SUBMITTED	********** //
+// *********************************************** //
+
+else if (isset($_POST['submit'])) {
+
+	if ($_POST['username'] == $username && $_POST['password'] == $password){
+	
+		//IF USERNAME AND PASSWORD ARE CORRECT SET THE LOG-IN SESSION
+		$_SESSION["login"] = $hash;
+		header("Location: $_SERVER[PHP_SELF]");
+		
+	} else {
+		
+		// DISPLAY FORM WITH ERROR
+		display_login_form();
+		echo '<p>Username or password is invalid</p>';
+		
+	}
+}	
 ?>
